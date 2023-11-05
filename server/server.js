e637c2dd37d597417cbad6d8d5494fab62ed306f
Ockyo <ockyo.0605@gmail.com>
2023-11-06 03:16:59 +0700

    write app.get for home page

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+
 const sql = require('mssql');
 const port = 4000;
 const http = require('http');
@@ -33,7 +34,7 @@ const cors = require('cors');
 const corsOptions = {
     origin: 'http://localhost:3000', // set origin specifically
     methods: 'GET,POST,PUT,DELETE', // set allow method http
-    optionsSuccessStatus: 204 
+    optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));// use middleware CORS and option config
@@ -42,10 +43,11 @@ app.post('/register', (req, res) => {
 
     //get value from request
     const { name, email, password } = req.body;
+    console.log(req.body);
     //create res
     // res.json({ message: 'Data sent and processed successfully' });
     //just sent res.json one time
-
+    const insertQuery = `INSERT INTO Users (Name, Email, Password) VALUES ('${name}', '${email}', '${password}')`;
 
 
     hashPassword(password, (hashPassword) => {
@@ -63,11 +65,76 @@ app.post('/register', (req, res) => {
         })
     });
 
+});
 
+// Xu ly post request cho route "/login"
+app.post('/login', (req, res) => {
+    //get value from req
+    // const { email, password } = req.body;
+    const { email, password } = req.body;
+    console.log(req.body);
+    const selectQuery = `SELECT * FROM Users WHERE Email = '${email}'`;
+    sql.query(selectQuery, (err, result) => {
+        if (err) {
+            console.error('Error while executing SQL query:', err);
+            res.status(500).json({ message: 'Error when querying the database' });
+        } else {
+            //check email in db
+            console.log(result.recordset);
+            if (result.recordset.length === 0) {
+                console.log('Invalid email or password.');
+                res.status(401).json({ message: 'Invalid email or password' });
+            } else {
+                // So sánh mật khẩu đã mã hóa trong cơ sở dữ liệu với mật khẩu đã cung cấp
+                bcrypt.compare(password, result.recordset[0].Password, (compareErr, isMatch) => {
+                    if (compareErr) {
+                        console.error('Error while comparing passwords:', compareErr);
+                        res.status(500).json({ message: 'Error when comparing passwords' });
+                    } else {
+                        if (isMatch) {
+                            // Mật khẩu khớp, có thể đăng nhập thành công
+                            console.log('Login successful');
+                            res.json({ message: 'Login successful' });
+                        } else {
+                            // Mật khẩu không khớp
+                            console.log('Invalid email or password.');
+                            res.status(401).json({ message: 'Invalid email or password' });
+                        }
+                    }
+                });
+            }
+        }
+    });
 });
+
+
+//get user info
+app.get('/user-info', (req, res) => {
+    // const email = g
+    console.log(email);
+    const selectQuery = `SELECT * FROM Users`;
+    
+    sql.query(selectQuery, (err, result) => {
+        if (err) {
+            console.error('Error while executing SQL query:', err);
+            res.status(500).json({ message: 'Error when querying the database' });
+        } else {
+            //check email in db
+            console.log(result.recordset);
+            if (result.recordset.length === 0) {
+                console.log('Invalid email or password.');
+                res.status(401).json({ message: 'Invalid email or password' });
+            }
+            
+            return res.status(200).json({ users: result.recordsets });
+        }
+    });
+
+})
 app.listen(port, () => {
-    console.log(`Listening in ${port}`);
+    console.log(`Server is running on ${port}`);
 });
+
 function hashPassword(password, callback) {
     const saltRounds = 10;
     bcrypt.hash(password, saltRounds, function (err, hashedPassword) {
