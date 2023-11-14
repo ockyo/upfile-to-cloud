d8ab9fbb717dfc22925f99001efbdd0b49d48913
Ockyo <ockyo.0605@gmail.com>
2023-11-15 03:33:26 +0700

    edit get user-info

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,42 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import FileUpload from './upfile';
 function Home() {
     const navigate = useNavigate();
+    const [username, setName] = useState('unknown')
+
     const handleLogout = () => {
-        navigate('/');
-    };
-    const [name, setName] = useState('unknown')
-    useEffect(() => {
-        fetch("https://localhost:4000/user-info")
-            .then(response => {
-                 if (response.valid === true) {
-                    setName(response.username);
-                }
-                else {
+        fetch('https://localhost:4000/logout', {
+            method: 'POST',
+            credentials: 'include', // Chắc chắn rằng cookie được gửi đi trong yêu cầu
+        })
+            .then(response => response.json())
+            .then(data => {
+                // Kiểm tra đăng xuất thành công
+                if (data.message === 'Logout successful') {
                     navigate('/');
                 }
             })
-            // .then(data => {
-            //     if (data.valid === true) {
-            //         setName(data.username);
-            //         navigate('/');
-            //     }
-            //     else {
-            //         navigate('/');
-            //     }
-            // })
+            .catch(error => console.error('Error:', error));
+
+    };
+
+
+    useEffect(() => {
+        fetch("https://localhost:4000/user-info", {
+            method: 'GET',
+            credentials: 'include',
+        })
+            .then(response => {
+                if (response.status === 200) {
+                    response.json().then(data => {
+                        setName(data.message);
+                    });
+                    navigate('/home');
+                }
+                // else if(response.status===401){
+                //     navigate('/');
+                // }
+                else {
+                    navigate('/')
+                }
+            })
             .catch(err => console.log(err))
     }, [])
 
     return (
-        <div>
-
-            <h1>HOME PAGE</h1>
-            <h1>Welcome {name}</h1>
-            <button onClick={handleLogout}>Logout</button>
+        <div className="container-home" >
+            <div className="header">
+                <div className="n-left">
+                    <div className="n-name">Cloud Fai</div>
+                </div>
+                <div className="nav-bar">
+                    <li>Home</li>
+                    <li>About us</li
+                    ><button onClick={handleLogout}>Logout</button>
+                </div>
+            </div>
+            <div className="content">
+                <h1>Welcome {username}</h1>
+            </div>
+            <FileUpload />
         </div>
     );
 };
-
 export default Home;
